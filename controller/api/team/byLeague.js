fcf29ffd87d15cf8676e1c5d70a672ddd8e459ed
Brian Kaufman <bpkaufman34@gmail.com>
2025-06-11 10:23:36 -0500

    calculate points

--- controller/api/team/byLeague.js
+++ controller/api/team/byLeague.js
@@ -10,116 +10,58 @@ router.get('/', (req, res) => {
   
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if(decoded) {
-      sequelize.query(
-        `SELECT * FROM (
-            SELECT
-                t.teamId,
-                t.name,
-                u.firstName,
-                u.lastName,
-                (
-                    (
-                        (
-                            SELECT
-                                count(*)
-                            FROM eliminationChoice ec
-                            INNER JOIN player p
-                            ON p.playerId = ec.playerId
-                            AND p.eliminatedId = ec.episodeId
-                            AND p.season = ${process.env.CURRENT_SEASON}
-                            WHERE t.teamId = ec.teamId
-                        ) * 5
-                    ) + (
-                        (
-                            SELECT 
-                                count(*)
-                            FROM immunityChoice ic
-                            LEFT JOIN immunity i ON i.tribeId = ic.tribeId
-                            LEFT JOIN immunity i2 on i2.playerId = ic.playerId
-                            LEFT JOIN player p on p.playerId = ic.playerId
-                            LEFT JOIN tribe trb on trb.tribeId = ic.tribeId
-                            WHERE (
-                                ic.tribeId = i.tribeId
-                                OR
-                                ic.playerId = i2.playerId
-                            )
-                            AND 
-                            (
-                                ic.episodeId = i.episodeId
-                                OR
-                                ic.episodeId = i2.episodeId
-                            )
-                            AND ic.teamId = t.teamId
-                            AND (
-                                p.season = ${process.env.CURRENT_SEASON}
-                                OR
-                                trb.season = ${process.env.CURRENT_SEASON}
-                            )
-                        ) * 2
-                    )
-                ) + t.bonus as \`bonus\`,
-                if(t.ownerId = '${decoded.id}', 1, 0) as isMine,
-                (
+        const playersHTML = `(
                     select 
-                        group_concat(concat(p1.firstName, ' - ', (select ifnull(sum(es1.points), 0) from episodeStatistic es1 where es1.playerId = pt1.playerId)) SEPARATOR '<br>') 
+                         
                     from playerTeam pt1 
                     left join player p1 on pt1.playerId = p1.playerId and p1.season = ${process.env.CURRENT_SEASON} 
                     where pt1.teamId = t.teamId
-                ) as playersHTML,
+                ) as playersHTML`
+
+        const columns = `,;`
+
+      sequelize.query(
+        `SELECT *, (cte2.basePoints + cte2.bonus) totalPoints 
+        FROM (
+            SELECT 
+                t.name,
+                t.ownerId,
+                t.teamId,
+                u.firstName,
+                u.lastName,
+                group_concat(concat(p.firstName, p.lastName, ' - ', cte.points)  SEPARATOR '<br>') as playersHTML,
+                SUM(cte.points) as basePoints,
                 (
-                    (
-                        (select 
-                            ifnull(sum(es.points), 0) 
-                            from episodeStatistic es 
-                            where es.playerId in (select pt.playerId from playerTeam pt inner join player p2 on p2.playerId = pt.playerId and p2.season = ${process.env.CURRENT_SEASON} where pt.teamId = t.teamId)
-                        ) * 1
-                    ) + (
-                        (
-                            SELECT
-                                count(*)
-                            FROM eliminationChoice ec
-                            INNER JOIN player p
-                            ON p.playerId = ec.playerId
-                            AND p.eliminatedId = ec.episodeId
-                            AND p.season = ${process.env.CURRENT_SEASON}
-                            WHERE t.teamId = ec.teamId
-                        ) * 5
-                    ) + (
-                        (
-                            SELECT 
-                                count(*)
-                            FROM immunityChoice ic
-                            LEFT JOIN immunity i ON i.tribeId = ic.tribeId
-                            LEFT JOIN immunity i2 on i2.playerId = ic.playerId
-                            LEFT JOIN player p on p.playerId = ic.playerId
-                            LEFT JOIN tribe trb on trb.tribeId = ic.tribeId
-                            WHERE (
-                                ic.tribeId = i.tribeId
-                                OR
-                                ic.playerId = i2.playerId
-                            )
-                            AND 
-                            (
-                                ic.episodeId = i.episodeId
-                                OR
-                                ic.episodeId = i2.episodeId
-                            )
-                            AND ic.teamId = t.teamId
-                            AND (
-                                p.season = ${process.env.CURRENT_SEASON}
-                                OR
-                                trb.season = ${process.env.CURRENT_SEASON}
-                            )
-                        ) * 2
-                    )
-                ) + t.bonus totalPoints
-            FROM team t
-            LEFT JOIN user u ON u.userId = t.ownerId
-            WHERE t.leagueId = '${req.query.leagueId}'
-        )
-        cte
-        ORDER BY totalPoints DESC
-        `,
+                    SELECT 
+                        SUM(q.points)
+                    FROM answerOption ao
+                    LEFT JOIN question q on q.questionId = ao.questionId
+                    INNER JOIN teamAnswer ta on ta.answerOptionId = ao.questionOptionId
+                    LEFT JOIN teamSurvey ts on ts.teamSurveyId = ta.teamSurveyId
+                    LEFT JOIN survey s on ts.surveyId = s.surveyId
+                    LEFT JOIN episode e on e.episodeId = s.episodeId
+                    WHERE ao.correct = true
+                        AND ts.teamId = t.teamId
+                        AND e.season = ${process.env.CURRENT_SEASON}
+                ) bonus
+            FROM (
+                SELECT
+                    es.playerId,
+                    SUM(es.points) AS points,
+                    pt.teamId
+                FROM episodeStatistic es
+                LEFT JOIN playerTeam pt on es.playerId = pt.playerId
+                LEFT JOIN team t ON t.teamId = pt.teamId
+                WHERE t.leagueId = '${req.query.leagueId}'
+                GROUP BY es.playerId, pt.teamId
+            ) cte
+            LEFT JOIN player p ON p.playerId = cte.playerId
+            LEFT JOIN team t on cte.teamId = t.teamId
+            LEFT JOIN user u on u.userId = t.ownerId
+            WHERE p.season = ${process.env.CURRENT_SEASON}
+            GROUP BY cte.teamId
+        ) cte2
+        ORDER BY totalPoints DESC`,
         {type: QueryTypes.SELECT}
       )
       .then(data => {
