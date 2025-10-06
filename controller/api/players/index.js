4f2946cd66e57ce68513b76ece4b54bd2938dc76
Brian Kaufman <bpkaufman34@gmail.com>
2025-10-06 16:46:33 -0500

    correct episode count for player

--- controller/api/players/index.js
+++ controller/api/players/index.js
@@ -28,7 +28,7 @@ router.get('/', (req, res) => {
             ],
             [
               sequelize.literal(`(
-                SELECT COUNT(*) 
+                SELECT COUNT(distinct episodeStatistic.episodeId) 
                 FROM episodeStatistic 
                 WHERE episodeStatistic.playerId = player.playerId
               )`),
